ac7df0aca630fb096dd83b463f2c4fc93fb101f8
hossames <100873739+hossames@users.noreply.github.com>
2024-07-23 22:01:16 +0300

    Update header.jsx

--- src/app/header.jsx
+++ src/app/header.jsx
@@ -26,11 +26,11 @@ const Menu = (props) =>{
 }
 
 const Nav =(props)=>{
-    const {lang,showMenu}=props;
+    const {lang}=props;
     console.log(lang);
     if(lang===0){
         return(
-        <nav  className={showMenu?'active links-Continer flex gap-1 text-xl font-medium text-blue-300':'links-Continer flex gap-1 text-xl font-medium text-blue-300'}>
+        <nav  className='links-Continer flex gap-1 text-xl font-medium text-blue-300'>
                 <a href="/ContactUs" className="p-3 nav-item nav-underline">{HeaderData[lang].Contact}</a>
                 <div className="products relative nav-item">
                     <span className="nav-underline p-3 flex items-center gap-1"><MdKeyboardArrowDown className=" text-2xl"></MdKeyboardArrowDown>{HeaderData[lang].Products}</span>
@@ -41,7 +41,7 @@ const Nav =(props)=>{
         )
     }else{
         return(
-        <nav className={showMenu?'active links-Continer flex gap-1 text-xl font-medium text-blue-300':'links-Continer flex gap-1 text-xl font-medium text-blue-300'}>
+        <nav className='links-Continer flex gap-1 text-xl font-medium text-blue-300'>
                 <a href="/" className="p-3 nav-item nav-underline">{HeaderData[lang].Home}</a>
                 <div className="products relative nav-item">
                     <span className="nav-underline p-3 flex items-center gap-1">{HeaderData[lang].Products}<MdKeyboardArrowDown   className=" text-2xl"></MdKeyboardArrowDown></span>
@@ -51,23 +51,67 @@ const Nav =(props)=>{
         </nav>)
     }
 }
+const SideBar = (props) => {
+    const [current,change]=useState(false);
+    let lang = props.lang;
+    return(
+        <>
+        <div className=' SideBar flex gap-1 text-xl font-medium text-blue-300'>
+                <a href="/" className="p-3 logo text-2xl uppercase font-semibold text-blue-300">Arta</a>
+                <div className="linkCon">
+                <a href="/" className="p-3 nav-item nav-underline">{HeaderData[lang].Home}</a>
+                <div className="nav-item">
+                    <span className="nav-underline p-3 flex items-center gap-1" onClick={ ()=>{change(!current);}
+                    }>{HeaderData[lang].Products}<MdKeyboardArrowDown   className=" text-2xl"></MdKeyboardArrowDown></span>
+                    {
+                    current?
+                    <div className="Drop flex flex-col nav-item">
+                        <a href="/products/single-disc-machines" className="menu-item">{HeaderData[lang].single_disc_machines}</a>
+                        <a href="/products/vacuum-cleaners" className="menu-item">{HeaderData[lang].vacuum_cleaners}</a>
+                        <a href="/products/scrubber-machines" className="menu-item">{HeaderData[lang].scrubber_machines}</a>
+                        <a href="/products/burnishers" className="menu-item">{HeaderData[lang].burnishers}</a>
+                        <a href="/products/sweepers" className="menu-item">{HeaderData[lang].sweepers}</a>
+                        <a href="/products/blowers" className="menu-item">{HeaderData[lang].blowers}</a>
+                        <a href="/products/cleaning-trolleys" className="menu-item">{HeaderData[lang].cleaning_trolleys}</a>
+                        <a href="/products/house-keeping-trolleys" className="menu-item">{HeaderData[lang].house_keeping_trolleys}</a>
+                        <a href="/products/laundry-trolleys" className="menu-item">{HeaderData[lang].laundry_trolleys}</a>
+                        <a href="/products/garbage-containers" className="menu-item">{HeaderData[lang].garbage_containers}</a>
+                        <a href="/products/cleaning-tools" className="menu-item">{HeaderData[lang].cleaning_tools}</a>
+                        <a href="/products/dryers" className="menu-item">{HeaderData[lang].dryers}</a>
+                        <a href="/products/bins" className="menu-item">{HeaderData[lang].bins}</a>
+                    </div>:''
+                    }
+                </div>
+                <a href="/ContactUs" className="p-3 nav-item nav-underline">{HeaderData[lang].Contact}</a>
+                </div>
+        </div>
+        </>
+    )
+}
 export const Header = (props)=>{
     const [lang, setLang] = useState(props.lang);
     const [showMenu,setShowMenu]=useState(false);
     return(
         <header id="header" className=" flex justify-between py-4 px-10 bg-black box-border items-center">
             <a href="/" className="p-3 logo text-2xl uppercase font-semibold text-blue-300">Arta</a>
-            <Nav lang={lang} showMenu={showMenu}/>
+            <Nav lang={lang}/>
             <div className="language-select text-xl font-medium text-blue-300 list-none flex gap-3">
                 <li  onClick={()=>(setLang(1))}>en</li>
                 <li onClick={()=>(setLang(0))}>ar</li>
             </div>
-            <RiMenu3Line className={showMenu?'text-blue-50 close icon' : 'text-blue-50 icon open'} style={{fontSize:'1.5rem'}} onClick={()=>{
+            {
+            showMenu?
+            <>
+            <RiCloseLine className='text-blue-50 icon close' style={{fontSize:'1.5rem'}} onClick={()=>{
                 setShowMenu(!showMenu);
-            }}></RiMenu3Line>
-            <RiCloseLine className={showMenu?'text-blue-50 open icon' : 'text-blue-50 icon close'} style={{fontSize:'1.5rem'}} onClick={()=>{
+            }}/>
+            <SideBar lang={lang}/>
+            </>
+            :
+            <RiMenu3Line className='text-blue-50 icon close' style={{fontSize:'1.5rem'}} onClick={()=>{
                 setShowMenu(!showMenu);
-            }}></RiCloseLine>
+            }}/>
+            }
         </header>
     )
 }
